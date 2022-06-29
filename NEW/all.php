ebf97c3a7f1e485b20050b9602e7c0690c810d50
Prasanna kumar A <90323500+PrasannaKumar-19@users.noreply.github.com>
2022-06-29 11:56:42 +0530

    Delete NEW directory

--- NEW/all.php
+++
@@ -1,8 +0,0 @@
-<?php
-	session_start();
-	        $host = "localhost";
-			$dbusername = "root";
-			$dbpassword = "";
-			$dbname = "hos";
-			$con = mysqli_connect($host, $dbusername, $dbpassword, $dbname);
-?>
--- NEW/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include 'all.php';
-    echo '<script>alert("Confirm logout");</script>';
-    session_unset();
-    session_destroy();
-    header("Location: login.php");
-    
-?>
